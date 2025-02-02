fd7550d07026c7a20846b2242e9363ebb8668332
Yuliia Hubska <zhukapuka10@gmail.com>
2025-02-02 18:43:48 +0200

    add

---
+++ js/Task-08.js
@@ -0,0 +1,20 @@
+// Напишіть функцію min(a, b), яка приймає 2 числа і повертає меньше з них.
+// Потрібно додати перевірку, що функція отримує саме числа, в іншому випадку
+// повертати з функції рядок - 'Not a number!'.
+// function min(a, b) {
+//   if (typeof a === "number" && typeof b === "number") {
+//     if (a > b) {
+//       return b;
+//     } else {
+//       return a;
+//     }
+//   } else {
+//     return "Not a nubmer";
+//   }
+// }
+function min(a, b) {
+  return typeof a !== "number" || typeof b !== "number"
+    ? "Not a number!"
+    : Math.min(a, b);
+}
+console.log(min("kh", 6));
